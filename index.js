d69925fe856f44fa0f9dd956d3c7a1d974bedb4e
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-03-15 07:44:26 +0300

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+// Function 1
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+console.log(findMatching(drivers, 'Bobby')); 
+
+// Function 2
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+console.log(fuzzyMatch(drivers, 'Sa'));
+
+// Function 3
+function matchName(driverArray, name) {
+    return driverArray.filter(driverObj => driverObj.name.toLowerCase() === name.toLowerCase());
+}
+
+
+const driverObjects = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Bobby', hometown: 'Tampa Bay' }
+];
+
+console.log(matchName(driverObjects, 'Bobby')); 
+
+
